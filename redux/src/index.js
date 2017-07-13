4853ec3ea127f956957755a93e8687b53a1b7d30
setoseng <setoseng@gmail.com>
2017-07-12 19:11:49 -0500

    added another react/redux practice app

---
+++ redux/src/index.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search'
+import SearchBar from './components/search_bar.js';
+import VideoList from './components/video_list.js';
+const API_KEY = 'AIzaSyAzTM4GAdj023X02Eaf8qK2kLU3P-pvVVQ';
+
+class App extends Component{
+  constructor(props){
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term:'surfboards'}, (videos) => {
+      this.setState({ videos });
+  //  this.setState({videos: videos}); ONLY WORKS WHEN KEY AND PROPERTY ARE SAME
+    });
+  }
+
+  render(){
+    return(
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App/>, document.querySelector('.container'));
